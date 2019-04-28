0d94a79bd2dd1b2e7ef5c2e418256f831716ee80
resourcex <mseman@resourcex.net>
2019-04-27 21:31:42 -0400

    updates

--- R/utils.R
+++ R/utils.R
@@ -50,6 +50,45 @@ create_empty_df<-function(n.cols,col.names){
 }
 
 
+#' colnameIDs_to_Names
+#'
+#' create an empty dataframe
+#' @param df.update number of columns
+#' @param df.LinkID_Name column names
+#' @param ID column names
+#' @param Name column names
+#' @export
+#' @examples
+#' colnameIDs_to_Names(n.cols,col.names)
+
+colnameIDs_to_Names<-function(df.update, df.LinkID_Name,ID,Name){
+
+
+  for(i in 1:nrow(df.LinkID_Name)){
+
+    colnames(df.update)[colnames(df.update)==df.LinkID_Name[i,ID]]<-df.LinkID_Name[i,Name]
+  }
+
+  return(df.update)
+}
+
+
+#' db_getCurLastUpdateTime
+#'
+#' get the time
+#' @export
+#' @examples
+#' db_getCurLastUpdateTime()
+
+db_getCurLastUpdateTime<- function() {
+  time1=Sys.time();
+  attr(time1, "tzone") <- "UTC"
+  as.character(Sys.time(), usetz = TRUE)
+}
+
+
+
+
 #' selectInput_filter
 #'
 #' subset a dataframe from select input
@@ -97,5 +136,56 @@ selectInput_filter<-function(df,filter,col.filter,all.option,by.rows=NULL,includ
 
   return(df)
 
+}
+
+
+#' selectInput_filter
+#'
+#' subset a dataframe from select input
+#' @param AppBudgetID dataframe to subset
+#' @param conexists input filter
+#' @param Div1Name column to filter on
+#' @param Div2Name include all term, example "All Available"
+#' @export
+#' @examples
+#' create_empty_df(n.cols,col.names)
+
+
+get_BudgetProgIDs<-function(AppBudgetID,conexists=NULL,Div1Name=values.setup$CostModelInfo$Div1Name,
+                            Div2Name=values.setup$CostModelInfo$Div2Name){
+
+  con<-conexists
+  if(is.null(conexists)){
+    con <- getClientCon()
+  }
+
+  #browser()
+  # Step 1 Get old item IDs
+  statement<-paste("SELECT ProgID FROM ProgBudgetInfo WHERE BudgetID IN ",create_IDstring(AppBudgetID),";",sep='')
+  ProgIDs<-dbGetQuery(con,statement)
+
+  if(nrow(ProgIDs)>0){
+
+    statement<-paste("SELECT * FROM ProgInfo WHERE ProgID IN ",create_IDstring(ProgIDs$ProgID),";",sep='')
+    Programs<-dbGetQuery(con,statement)
+
+
+  }else{
+
+    col.names<-c("ProgID", "CostModelID","ProgNum","ProgName","ProgNameShort","ServiceType", "ScoringType","ProgActive", "ProgDept", "ProgDiv" ,
+                 "ProgFund","ProgDescription","ProgComments","ProgGroup","ProgSubGroup","AssignedRevenue", "AssignedAllocatedCost", "AssignedFixedCost",
+                 "FinalScore","Quartile","ProgLastUpdated","ProgUser")
+    Programs<-create_empty_df(n.cols=22,col.names)
+
+
+  }
+
+  colnames(Programs)[c(3,4,6,7,9,10,11,12,13)]<-c('ProgramNumber','Program','Type','Fixed',Div1Name,Div2Name,'Fund','Description','Comments')
+
+
+  if(is.null(conexists))(dbDisconnect(con))
+
+
+  return(Programs)
 }
 
